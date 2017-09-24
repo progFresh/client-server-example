08e73a4725cb4b8061c20baf3579a745aabfd589
Сергей Полозов <fresher40@gmail.com>
2017-09-25 02:18:29 +0300

    Инициализация проекта

---
+++ Robots/Code/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Robots
+//
+//  Created by Сергей Полозов on 25.09.17.
+//  Copyright © 2017 sergey. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+        ) -> Bool {
+        return true
+    }
+}
+
---
+++ Robots/Code/Presentation/Common/ViewController/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Robots
+//
+//  Created by Сергей Полозов on 25.09.17.
+//  Copyright © 2017 sergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
